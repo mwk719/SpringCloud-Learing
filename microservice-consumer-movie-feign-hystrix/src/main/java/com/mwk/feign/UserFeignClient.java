ab51044cb08c61c4a3a196d6e00fa46a991a86ea
闵渭凯 <1294608448@qq.com>
2018-07-18 14:26:52 +0800

    feign默认hystrix

---
+++ microservice-consumer-movie-feign-hystrix/src/main/java/com/mwk/feign/UserFeignClient.java
@@ -0,0 +1,17 @@
+package com.mwk.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mwk.entity.User;
+
+@FeignClient(name = "microservice-provider-user-client", fallback = HystrixClientFallback.class)
+public interface UserFeignClient {
+
+	// 支持负载均衡
+	@RequestMapping(value = "/simple/{id}", method = RequestMethod.GET)
+	public User findById(@PathVariable("id") Long id);
+
+}
--- microservice-consumer-movie-ribbon-hystrix/src/main/java/com/mwk/controller/MovieController.java
+++ microservice-consumer-movie-ribbon-hystrix/src/main/java/com/mwk/controller/MovieController.java
@@ -8,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 
 import com.mwk.entity.User;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @RestController
 public class MovieController {
@@ -17,8 +16,10 @@ public class MovieController {
 	private RestTemplate restTemplate;
 
 	@GetMapping("movie/{id}") // 回滚在同一进程
-	@HystrixCommand(fallbackMethod = "findByIdFallback", commandProperties = {
-			@HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE") })
+	@HystrixCommand(fallbackMethod = "findByIdFallback")
+	// , commandProperties = {
+	// @HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE")
+	// })
 	public User findById(@PathVariable Long id) {
 		return this.restTemplate.getForObject("http://microservice-provider-user-client/simple/" + id, User.class);
 	}
@@ -29,8 +30,7 @@ public class MovieController {
 	 * @param id
 	 * @return
 	 */
-	@SuppressWarnings("unused")
-	private User findByIdFallback(Long id) {
+	public User findByIdFallback(Long id) {
 		User u = new User();
 		u.setId(0L);
 		u.setUsername("回滚");
