1ca3c0a540810662a5e180908eefc52252da189d
闵渭凯 <1294608448@qq.com>
2018-07-18 10:15:08 +0800

    同一进程回滚

--- microservice-consumer-movie-ribbon-hystrix/src/main/java/com/mwk/controller/MovieController.java
+++ microservice-consumer-movie-ribbon-hystrix/src/main/java/com/mwk/controller/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.mwk.entity.User;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @RestController
 public class MovieController {
@@ -15,20 +16,22 @@ public class MovieController {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@GetMapping("movie/{id}")
-	@HystrixCommand(fallbackMethod="findByIdFallback")
+	@GetMapping("movie/{id}") // 回滚在同一进程
+	@HystrixCommand(fallbackMethod = "findByIdFallback", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE") })
 	public User findById(@PathVariable Long id) {
 		return this.restTemplate.getForObject("http://microservice-provider-user-client/simple/" + id, User.class);
 	}
 
 	/**
 	 * 回滚方法
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@SuppressWarnings("unused")
 	private User findByIdFallback(Long id) {
-		User u=new User();
+		User u = new User();
 		u.setId(0L);
 		u.setUsername("回滚");
 		return u;
